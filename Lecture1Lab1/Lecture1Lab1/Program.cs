d285e266dc9ae1b2d6c67fb9133c155c9baefff4
KMLEWIS <kmlewis@miracosta.edu>
2017-08-21 20:28:55 -0700

    full lab

--- Lecture1Lab1/Lecture1Lab1/Program.cs
+++ Lecture1Lab1/Lecture1Lab1/Program.cs
@@ -10,9 +10,24 @@ namespace Lecture1Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello CS155.");
-            Console.WriteLine("Commit and PUSH");
+            //   print a  message to   the screen
+            Console.WriteLine("Hello out there.");
+            Console.WriteLine("I will add two numbers for you.");
+            Console.WriteLine("Enter one number and press enter, thenenter second number and press enter:");
 
+            //   declare two integer variables
+            int n1, n2, sum;
+            string userInput;
+            userInput =  Console.ReadLine();
+            n1   =  Int32.Parse(userInput);
+            n2   =  Int32.Parse(Console.ReadLine());
+
+            //   calculate the sum of   these two numbers
+            sum = n1   +  n2;
+            //   print a  message along with the sum of the two numbers
+            Console.WriteLine("The sum of those two numbers is");
+            Console.WriteLine(sum);
+            //Just to   pause the screen.
             Console.ReadLine();
         }
     }
