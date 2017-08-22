70fa526d2cc32b0f4c4a4b5b14c626de10aaf12a
KMLEWIS <kmlewis@miracosta.edu>
2017-08-21 20:21:19 -0700

    Adding Lecture1Lab1.

---
+++ Lecture1Lab1/Lecture1Lab1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture1Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello CS155.");
+
+            Console.ReadLine();
+        }
+    }
+}
