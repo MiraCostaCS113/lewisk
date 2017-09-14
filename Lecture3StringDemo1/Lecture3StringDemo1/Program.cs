a05f8ec0c0ccda3fe9f9afffd9033fdbde487a4a
KMLEWIS <kmlewis@miracosta.edu>
2017-09-13 20:45:16 -0700

    Lecture 3 In Class work

--- Lecture3StringDemo1/Lecture3StringDemo1/Program.cs
+++ Lecture3StringDemo1/Lecture3StringDemo1/Program.cs
@@ -11,28 +11,47 @@ namespace Lecture3StringDemo1
         const string ENDING = "ay";
         static void Main(string[] args)
         {
-            string firstName = "Kevin";
-            firstName = firstName.ToLower(); //make it all lowercase
-            string firstLetter = firstName.Substring(0, 1); //separate out the first letter
-            string secondLetter = firstName.Substring(1, 1); //separate out the second letter
-            string remainderFirstName = firstName.Substring(2); //seperate out the rest of the first name
 
-            string pigLatinFirstName = secondLetter.ToUpper() + remainderFirstName + firstLetter + ENDING;
+            //Console.WriteLine("{0}{1}{2}{0}{1}", "A", "B", "C");
 
-            Console.WriteLine("Kevin in pig latin is {0}", pigLatinFirstName);
 
-            string lastName = "Lewis";
-            lastName = lastName.ToLower(); //make it all lowercase
-            string firstLNLetter = lastName.Substring(0, 1); //separate out the first letter
-            string secondLNLetter = lastName.Substring(1, 1); //separate out the second letter
-            string remainderLasName = lastName.Substring(2); //seperate out the rest of the first name
+ 
 
-            string pigLatinLastName = secondLNLetter.ToUpper() + remainderLasName + firstLNLetter + ENDING;
+            Console.WriteLine("K in pig latin is {0}", PigLatin("K"));
 
-            Console.WriteLine("Lewis in pig latin is {0}", pigLatinLastName);
+
+
+            Console.WriteLine("LE in pig latin is {0}", PigLatin("LE"));
 
             Console.ReadLine();
 
         }
+
+        static string PigLatin(string name)
+        {
+            if (name.Length == 1)
+            {
+                return name.ToUpper() + ENDING;
+            }
+            else if (name.Length == 2)
+            {
+                return name + ENDING;
+
+            }
+            else
+            {
+                name = name.ToLower();
+                string firstLetter = name.Substring(0, 1);
+                string secondLetter = name.Substring(1, 1).ToUpper();
+                string remainderName = name.Substring(2);
+
+                return secondLetter + remainderName + firstLetter + ENDING;
+            }
+
+
+
+        }
     }
+
+    
 }
---
+++ Lecture3Switch/Lecture3Switch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your vehicle type:");
+            Console.WriteLine("1 - passenger");
+            Console.WriteLine("2 - bus");
+            Console.WriteLine("3 - truck");
+            Console.WriteLine("4+ - other");
+
+            int input = Convert.ToInt32(Console.ReadLine());
+            double charge;
+
+            switch (input)
+            {
+                case 1:
+                    charge = 1.5;
+                    break;
+                case 2:
+                    charge = 2.5;
+                    break;
+                case 3:
+                    charge = 4.5;
+                    break;
+                default:
+                    charge = 5;
+                    break;
+            }
+            Console.WriteLine("Please pay {0:C}", charge);
+            Console.ReadLine();
+        }
+    }
+}
