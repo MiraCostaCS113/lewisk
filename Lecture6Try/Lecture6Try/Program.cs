585126d421d0305bef6abbd963b26f8c6d2d8468
KMLEWIS <kmlewis@miracosta.edu>
2017-10-01 15:38:49 -0700

    Exception handling demos

---
+++ Lecture6Try/Lecture6Try/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture6Try
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scoreList = new int[10];
+            try
+            {
+                Console.WriteLine("We are initializing the array.");
+                //let's initialize my array
+                for (int i = 0; i <= 10; i++)
+                {
+                    scoreList[i] = 1;
+                }
+
+            }
+            //this one must come after the specific ones. 
+            catch (IndexOutOfRangeException e) //a specific exception
+            {
+                Console.WriteLine("Something bad with the array probably happened." + e);
+            }
+            catch (Exception e) // this is the general exception
+            {
+                Console.WriteLine("Something bad happened." + e);
+            }
+            finally
+            {
+                Console.WriteLine("We are done");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lecture6Try2/Lecture6Try2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture6Try2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isGood = false;
+
+            Console.WriteLine("Enter number between 1 and 100:");
+            while (!isGood)
+            {
+                string strNumber = Console.ReadLine();
+                int number = 0;
+                try
+                {
+                    number = Convert.ToInt32(strNumber);
+                    isGood = true;
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Bad Input./nEnter number between 1 and 100:");
+                }
+            }
+            Console.WriteLine("All's good.");
+            Console.ReadLine();
+        }
+    }
+}
