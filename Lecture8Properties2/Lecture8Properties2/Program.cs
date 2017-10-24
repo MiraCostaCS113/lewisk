af4a89a788a0d955830f39fc35a4c34f511d645f
KMLEWIS <kmlewis@miracosta.edu>
2017-10-23 18:23:17 -0700

    Lecture8 Properties ture 7

---
+++ Lecture8Properties2/Lecture8Properties2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture8Properties2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnixTime time = new UnixTime();
+            time.Seconds = -84897498;
+            Console.WriteLine("Seconds: {0}", time.Seconds);
+
+            time.Seconds = 11334243;
+            Console.WriteLine("Seconds: {0}", time.Seconds);
+            Console.WriteLine("Minutes: {0}", time.Minutes);
+            time.Event = "Mira Costa Built.";
+            Console.WriteLine("Event: {0}", time.Event);
+
+            Console.ReadKey();
+        }
+    }
+}
